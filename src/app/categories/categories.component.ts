a37f3e4701e840d117c272fb5abc5169fd18864a
Webstreet <64398429+webstreet-cie@users.noreply.github.com>
2020-05-10 15:00:54 +0200

    Chapter 3 - Exercises

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -22,3 +22,4 @@ export class CategoriesComponent implements OnInit {
   }
 
 }
+
--- src/app/trainings/trainings.component.ts
+++ src/app/trainings/trainings.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TrainingsService } from './trainings.service';
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-trainings',
@@ -20,8 +21,95 @@ export class TrainingsComponent implements OnInit {
   }
 
   ngOnInit() {
+    var message = "Welcome students!";
+    log(message);
   }
 
 }
+
+function log(message) {
+  console.log(message);
+}
+
+function iteration(){
+  for (let i=0;i<5;i++) {
+    console.log(i);
+  }
+  //console.log("Final Result:" + i);
+}
+
+iteration();
+
+const country = "France";
+//country = "Great-Britain";
+
+const countries = ["France", "Ireland"];
+countries.push("Switzerland");
+
+const towns = {name: "Paris"};
+towns.name = "New-York";
+
+let hello;
+hello = "hello students!";
+hello = true;
+hello = 1;
+
+interface Module {
+  title:string;
+  items: string[];
+}
+
+function createModule(module: Module): {title: string; items:string[]} {
+  return module;
+}
+
+let module1 = createModule(
+  {
+    title: "The basics of HTML",
+    items:["Meta tags","Heading tags", "paragraphs"]
+  }
+);
+
+console.log(module1);
+
+interface Training {
+  display():void;
+}
+
+export class PayingTraining implements Training {
+  constructor(
+    private _title : string, 
+    private description : string,
+    private price?: number,
+  
+    ) {
+  }
+
+  get title() {
+    return this._title;
+  }
+
+  set title(title) {
+    this._title = title;
+  }
+
+  display(): void {
+    console.log(`Welcome to this training: ${this.title},  ${this.description}, ${this.price} $`);
+  }
+
+  calculatePricePerModule(price,moduleNumber) {
+    let pricePerModule = price / moduleNumber;
+    return pricePerModule;
+  }
+}
+
+let angularTraining = new PayingTraining(
+  "Angular - Create Web Applications", 
+  "Learn to create web apps with Angular", 
+  1490);
+  angularTraining.title = "fake title";
+  angularTraining.display();
+
+  console.log("The price per module is: " + angularTraining.calculatePricePerModule(1490, 7));
 
 
